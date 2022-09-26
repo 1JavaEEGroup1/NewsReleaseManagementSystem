47bf2d2d5f0774686c4f022a34b2c021a256d22b
jhlyh <1952457639@qq.com>
2022-09-26 17:04:15 +0800

    创建项目

---
+++ src/main/java/com/example/newsreleasemanagementsystem/NewsReleaseManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.newsreleasemanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsReleaseManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewsReleaseManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/newsreleasemanagementsystem/NewsReleaseManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.newsreleasemanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsReleaseManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
